3136ab850aeeb9f957b74bcf3e07d3a40e32fa23
mahmed-eng <57319953+mahmed-eng@users.noreply.github.com>
2022-08-25 10:42:46 +0500

    Add files via upload
    Branch added

---
+++ Database/Scripts/Branch.sql
@@ -0,0 +1,42 @@
+USE [SMS]
+GO
+
+/****** Object:  Table [dbo].[Company]    Script Date: 25/08/2022 10:02:57 am ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'TheSchema' 
+                 AND  TABLE_NAME = 'Branch_tb'))
+BEGIN
+    --Do Stuff
+
+
+
+CREATE TABLE [dbo].[Branch_tb](
+	[CompanyId] [int] NOT NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](200) NULL,
+	[Address] [varchar](300) NULL,
+	[Email] [varchar](100) NOT NULL,
+	[Phone] [varchar](100) NOT NULL,
+	[Fax] [nvarchar](50) NOT NULL,
+	[Cell] [varchar](50) NOT NULL,
+	[RegisterNumber] [varchar](50) NOT NULL,
+	[CreatedBy] [int] NOT NULL,
+	[CreatedDate] [datetime] NULL,
+	[UpdatedBy] [int] NOT NULL,
+	[UpdatedDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+
+
