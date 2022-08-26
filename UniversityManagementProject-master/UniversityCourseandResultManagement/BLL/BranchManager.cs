36607e972b895d35fcf38969f84ef2a27969416e
mahmed-eng <57319953+mahmed-eng@users.noreply.github.com>
2022-08-26 17:14:12 +0500

    branchmanager
    added

---
+++ UniversityManagementProject-master/UniversityCourseandResultManagement/BLL/BranchManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UniversityCourseandResultManagement.Gateway;
+using UniversityCourseandResultManagement.Models;
+
+namespace UniversityCourseandResultManagement.BLL
+{
+    public class BranchManager
+    {
+        BranchGateway aBranchGateway = new BranchGateway();
+
+        public string Save(Branch aBranch)
+        {
+            if (aBranchGateway.Save(aBranch) > 0)
+            {
+                return "Branch Save Successfully!!";
+            }
+            else
+            {
+                return "Failed";
+            }
+        }
+
+        public List<Branch> GetAllBranch()
+        {
+            return aBranchGateway.GetAllBranch();
+        }
+    }
+}
