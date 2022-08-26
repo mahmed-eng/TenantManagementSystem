99106d088df2f82e29bb907ed1fd4fa88ce828e2
mahmed-eng <57319953+mahmed-eng@users.noreply.github.com>
2022-08-26 17:12:28 +0500

    agentmanager
    added

---
+++ UniversityManagementProject-master/UniversityCourseandResultManagement/BLL/AgentManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UniversityCourseandResultManagement.Gateway;
+using UniversityCourseandResultManagement.Models;
+
+namespace UniversityCourseandResultManagement.BLL
+{
+    public class AgentManager
+    {
+        AgentGateway aAgentGateway = new AgentGateway();
+
+        public string Save(Agent aAgent)
+        {
+            if (aAgentGateway.Save(aAgent) > 0)
+            {
+                return "Agent Save Successfully!!";
+            }
+            else
+            {
+                return "Failed";
+            }
+        }
+
+        public List<Agent> GetAllAgent()
+        {
+            return aAgentGateway.GetAllAgent();
+        }
+    }
+}
